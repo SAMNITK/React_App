07b3d332d8d902c33c828e69192206307d6de605
Satnam Singh <samsp086@gmail.com>
2021-08-09 10:31:07 +0530

    Updates

---
+++ precache-manifest.526454e319bbba7637a3da23493d1a08.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55605813345ae22ad27c8f2f66721be2",
+    "url": "/react-website-v1-master/index.html"
+  },
+  {
+    "revision": "9920f1b4b04b33eb455d",
+    "url": "/react-website-v1-master/static/css/main.6d0a3482.chunk.css"
+  },
+  {
+    "revision": "04b8d5196e92a76bb111",
+    "url": "/react-website-v1-master/static/js/2.612d149b.chunk.js"
+  },
+  {
+    "revision": "55b79ebb5e774a9f9242c9bbc135e8e0",
+    "url": "/react-website-v1-master/static/js/2.612d149b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9920f1b4b04b33eb455d",
+    "url": "/react-website-v1-master/static/js/main.e3f77013.chunk.js"
+  },
+  {
+    "revision": "0821999f65f9848cea19",
+    "url": "/react-website-v1-master/static/js/runtime-main.f65cb542.js"
+  },
+  {
+    "revision": "77d0256869d0cc0e1b6e3b7b6ab36522",
+    "url": "/react-website-v1-master/static/media/01.77d02568.jpg"
+  },
+  {
+    "revision": "c383e6cc72ff1e1123b6cae01bd3ed22",
+    "url": "/react-website-v1-master/static/media/50.c383e6cc.jpg"
+  },
+  {
+    "revision": "408cae7280c8f9c6842c3c40d25f970d",
+    "url": "/react-website-v1-master/static/media/51.408cae72.jpg"
+  },
+  {
+    "revision": "6336b7eb14909ab681313d5007975f9f",
+    "url": "/react-website-v1-master/static/media/52.6336b7eb.jpg"
+  },
+  {
+    "revision": "2556759c55b5e66b4e0baa8527ed2ca9",
+    "url": "/react-website-v1-master/static/media/53.2556759c.jpg"
+  },
+  {
+    "revision": "7e04b60e00d4ccf51b87b991dbacdcd6",
+    "url": "/react-website-v1-master/static/media/54.7e04b60e.jpg"
+  },
+  {
+    "revision": "65947321a7b55274287a5bcd958bf48f",
+    "url": "/react-website-v1-master/static/media/Logo.65947321.png"
+  }
+]);
